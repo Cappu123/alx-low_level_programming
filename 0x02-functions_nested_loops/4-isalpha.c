4006f28ff60e5de11564e4d7af4647b5a843afbd
Cappu123 <cappyalemayehu12@gmail.com>
2023-08-17 06:41:24 -0700

    alpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+	for (r = 65; r <= 90; r++) || for (r = 97; r <= 122; r++)
+	{
+		_putchar(1);
+	}
+	{
+		_putchar(0);
+	}
+	return;
+}
