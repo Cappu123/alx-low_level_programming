1ca712b1e2f3c6d1899a6f01b9b7e5538ba138a9
Cappu123 <cappyalemayehu12@gmail.com>
2023-08-17 03:48:37 -0700

    bettystyle

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,16 +5,15 @@
  * Returns 0 otherwise
  *
  */
-int c;
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 	_putchar('\n');
 }
