52861d5f5f9f1b63d229cd7dee322123ad79429b
Cappu123 <cappyalemayehu12@gmail.com>
2023-08-18 20:39:21 -0700

    last trial

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*This function prints the numbers from 0 to 9,
+ * followed by a new line
+ * 
+ */
+
+void print_numbers(void)
+{
+	int a;
+	if (a >= 0 && a<=9)
+	{
+		_putchar("a")
+		_putchar("\n");
+	}
+	return (0);
+}
