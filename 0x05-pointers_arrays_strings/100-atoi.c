60679f86f55f49a4aa06fcb6086cdcd1245d690e
Cappu123 <cappyalemayehu12@gmail.com>
2023-08-22 17:00:03 -0700

    betty style code

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,7 +18,7 @@ for (i = 0; s[i] != '\0'; i++)
 			if (s[i] == '-')
 				test = -test;
 	if (s[i] >= '0' && s[i] <= '9')
-		n = (10 * n) + test *(s[i] - '0');
+		n = (10 * n) + test * (s[i] - '0');
 	else if (n)
 		break;
 }
