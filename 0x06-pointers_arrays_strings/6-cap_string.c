c3d123418f890984362f3e60239a2df35ee9bb7f
Cappu123 <cappyalemayehu12@gmail.com>
2023-08-24 18:38:35 -0700

    idh

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,13 +9,13 @@ char *cap_string(char *str)
 {
 	int i;
 	int j;
-	charc[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
+	char[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 	i = 0;
 
 	while (str[i] != '\0')
 	{
 		if (i == 0 && str[i] >= 97 && str[i] <= 122)
-		{
+		{		{		
 			str[i] = str[i] - 32;
 		}
 
@@ -24,13 +24,12 @@ char *cap_string(char *str)
 		while (c[j] != '\0')
 			{
 				if (c[j] == str[i] && (str[i+1] >= 97 && str[i+1] <= 122))
-				{
-					str[i+1] = str[i+1] - 32;
+				{					str[i+1] = str[i+1] - 32;
 				}
 				j++;
-			}
+			}		
 		i++;
+		}
+		return (str);
 	}
-	return (str);
 }
-
