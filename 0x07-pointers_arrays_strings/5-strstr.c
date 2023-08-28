889afc2865f16307727fd0447053f154fbce5ef5
Cappu123 <cappyalemayehu12@gmail.com>
2023-08-28 16:18:43 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strstr - a function that locates a substring
+ * @haystack: a string
+ * @needle: a string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	for (i = 0; haystack[i] != '\0';)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (needle[j] != haystack[i + j])
+			{
+				break;
+			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
+		}
+		i++;
+	}
+	return ('\0');
+}
