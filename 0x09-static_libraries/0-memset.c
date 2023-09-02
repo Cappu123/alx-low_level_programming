ce16ecf4f8aa985d61a07f02614818d949e57076
Cappu123 <cappyalemayehu12@gmail.com>
2023-09-02 05:13:23 -0700

    last

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * -memset-fill a block of memory with a specific value
- *  @s: starting address of memory to be filled
- *  @b: the desired value
- *  @n: number of bytes to be changed
+ * _memset-fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changedd
  *
- *  Return: cahnged array with new value for n bytes
+ * Return: changed array with new valuie for n bytes
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -16,5 +16,7 @@ char *_memset(char *s, char b, unsigned int n)
 		s[i] = b;
 		n--;
 	}
-	return(s);
+	return (s);
 }
+
+
