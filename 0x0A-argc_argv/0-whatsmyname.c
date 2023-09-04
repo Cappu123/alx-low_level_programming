0d2d0c9fa626793aab21aed541dad39116734f21
Cappu123 <cappyalemayehu12@gmail.com>
2023-09-04 16:21:46 -0700

    argv[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc - an integer which counts the number of program
+ * that runs on the command line
+ * @argv - an array of strings which outputs the name of th
+ * program
+ * 
+ * Return: 0 (Always Success)
+ */
+
+int main(int argc, char* argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return 0;
+}
