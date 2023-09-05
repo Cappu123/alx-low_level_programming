c8a818c06ecc0d02d9275ca1bc109f2091c41a32
Cappu123 <cappyalemayehu12@gmail.com>
2023-09-05 13:54:40 -0700

    upto 2

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
-#ifdef Main_H
+#define Main_H
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
+char i*str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
