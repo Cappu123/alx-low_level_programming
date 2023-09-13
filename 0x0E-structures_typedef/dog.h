ac32b7c20528928cf2a757a3d5dd3c3170b61651
Cappu123 <cappyalemayehu12@gmail.com>
2023-09-12 19:57:54 -0700

    last

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,28 @@
-#include <stdlib.h>
-#include "dog.h"
+#ifndef FOG_H
+#define DOG_H
 
 /**
- * struct dog - such structure, wow
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
+ * struct dog - A new type desctibing a dog
+ * @name: the name of dog
+ * @age: the age of dog
+ * @owner: the owner of dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+/**
+ * dog_t -typedef for struct dog
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
+typedef struct dog _dog_t;
+
+void ini_dog(struct dog * d, char * name, float age, char * owner);
+void print_dog(struct dog * d);
+dog_t * new_dog(char * name, float age, char * owner);
+void free_dog(dog_t * d);
+
+#endif
