3e22ea096a3d9ac663c5acad7856813993b86587
Cappu123 <cappyalemayehu12@gmail.com>
2023-09-12 19:59:29 -0700

    kd

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,9 +20,9 @@ struct dog
 
 typedef struct dog _dog_t;
 
-void ini_dog(struct dog * d, char * name, float age, char * owner);
-void print_dog(struct dog * d);
-dog_t * new_dog(char * name, float age, char * owner);
-void free_dog(dog_t * d);
+void ini_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char * wner);
+void free_dog(dog_t *d);
 
 #endif
