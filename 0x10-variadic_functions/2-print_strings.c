4add6e3235b6f7ce647313faf09c4a4b9644bc2f
Cappu123 <cappyalemayehu12@gmail.com>
2023-09-14 19:39:03 -0700

    lastt

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_strings - prints strings with separator
@@ -30,3 +32,4 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s%s", separator, str);
 	}
+}
