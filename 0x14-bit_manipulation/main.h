91e4444ce31ae3c1a46dfb4bc7332edc146e2d74
Cappu123 <cappyalemayehu12@gmail.com>
2023-09-29 15:03:00 -0700

    bits

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,15 @@
+#ifndef "__MAIN_H__
+#define "__MAIN_H__
+
+
+
+unsigned int binary_to_unit(cost char *b);
+void print_binary(unsigned long int m);
+int_putchar(char c);
+int get_bit(unsigned long int n, unsigned int index;
+set_bit(unsigned long int *n, insigned ant index;
+int clear_clear_bit(unsigned long int *n, unsigned int index;
+	unsigned int flip_bits(unsigned long int n, unsigned long int m);
+	int get_endiannes(void);
+
+#endif
