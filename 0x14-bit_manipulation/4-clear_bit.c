8ca53ed1704e28941bf5add4027c4185356f1064
Cappu123 <140403056+Cappu123@users.noreply.github.com>
2023-10-26 23:33:03 +0300

    Create 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+/*
+* clear_bit - sets the value of a bit to 0 at a gicen index.
+* @n: pointer of an unsigned long int;
+* @index: index of the bit
+* 
+* Return: 1 if it works, -1 if it doesnot
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int l;
+
+	if (index > 63)
+		return (-1);
+	l = 1 << index;
+
+	if (*n & *l)
+		*n ^= l;
+	return (1);
+}
