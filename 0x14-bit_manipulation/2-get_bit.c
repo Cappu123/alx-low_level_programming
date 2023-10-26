1feae42f4d103ca087df0209f2b6405b0548d3d7
Cappu123 <140403056+Cappu123@users.noreply.github.com>
2023-10-27 00:00:07 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,10 +1,14 @@
-/*
-* get_bit - returns the value of a bit at a given index
-* @n: unsigned long int input
-* @index: index of the bit
-* Return: value of the bit
-*/
+#include "main.h"
 
+
+/**
+ * get_bit - returns the value of a bit at a given
+ * index.
+ * @n: unsigned long int input.
+ * @index: index of the bit.
+ *
+ * Return: value of the bit.
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
