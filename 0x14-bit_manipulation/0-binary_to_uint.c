8808c65187f9a6e59aead9b4e2845948a8b65a5d
Cappu123 <cappyalemayehu12@gmail.com>
2023-10-27 14:02:46 -0700

    task0- update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,19 +7,20 @@
  *
  * Return: unsigned int.
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int un_i;
+	unsigned int uin;
 	int len, base_two;
 
 	if (!b)
 		return (0);
 
-	un_i = 0;
+	uin = 0;
 
 	for (len = 0; b[len] != '\0'; len++)
 		;
-
+	
 	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
@@ -29,7 +30,9 @@ unsigned int binary_to_uint(const char *b)
 
 		if (b[len] & 1)
 		{
-			un_i += base_two;
+			uin += base_two;
 		}
 	}
 
+	return (uin);
+}
