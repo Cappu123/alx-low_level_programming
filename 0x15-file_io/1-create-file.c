4032ebc211e6a2f5166d1ede48f41da23843b942
Cappu123 <140403056+Cappu123@users.noreply.github.com>
2023-10-30 23:34:30 +0300

    Update 1-create-file.c

--- 0x15-file_io/1-create-file.c
+++ 0x15-file_io/1-create-file.c
@@ -9,9 +9,9 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int fd;
+	int nletters;
 	int rwr;
-  int fd;
-	int num_letters;
 
 	if (!filename)
 		return (-1);
@@ -24,10 +24,10 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (num_letters = 0; text_content[num_letters]; num_letters++)
+	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	rwr = write(fd, text_content, num_letters);
+	rwr = write(fd, text_content, nletters);
 
 	if (rwr == -1)
 		return (-1);
