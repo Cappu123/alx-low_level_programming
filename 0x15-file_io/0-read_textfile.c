ec851bc792e4233f5f63d15fdb8de91017ca41d4
Cappu123 <140403056+Cappu123@users.noreply.github.com>
2023-10-30 23:38:04 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <unistd.h>
 #include <sys/types.h>
-#include <stdlib.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
@@ -14,30 +14,30 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd;
+	int fd;
 	ssize_t lenr, lenw;
-	char *buff;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
 		close(fd);
-    return (0);
+		return (0);
 	}
-	lenr = read(fd, buff, letters);
+	lenr = read(fd, buffer, letters);
 	close(fd);
 	if (lenr == -1)
 	{
-		free(buff);
+		free(buffer);
 		return (0);
 	}
-	lenw = write(STDOUT_FILENO, buff, lenr);
-	free(buff);
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
 	if (lenr != lenw)
 		return (0);
 	return (lenw);
