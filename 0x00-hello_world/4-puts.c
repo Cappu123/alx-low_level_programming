520b67ad4be2bb8f36b5aa6a22ee614284003cf2
Cappu123 <cappyalemayehu12@gmail.com>
2023-11-17 03:39:38 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(const char *s)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	*s = "Programming is like building a multilingual puzzle";
+	puts(s);
+	puts(\n);
+	return (0);
 }
