3ee05de685ff1c21dfad608daedc0d7411dd2a83
Cappu123 <cappyalemayehu12@gmail.com>
2023-11-18 04:56:29 -0800

    commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,19 +10,23 @@ int main(void)
 	int c, i;
 
 	for (c = 0; c <= 9; c++)
-	for (i = 0; i <= 9; i++)
-	if (c < i)
 	{
-		putchar(c);
-		putchar(i);
+
+		for (i = 0; i <= 9; i++)
+		{
 	
-		if (c == 8 || i == 9)
-			continue;
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
+	
+				if (c == 8 || i == 9)
+				continue;
 			
-		putchar(',');
-		putchar(' ');
-		putchar('\n');
+				putchar(',');
+				putchar(' ');
+			}
 		}
-	}
-	return (0);
+		putchar('\n');
+		return (0);
 }
