b814dbaa430a870735f2fa1edae4b9471d9837a9
Cappu123 <cappyalemayehu12@gmail.com>
2023-11-23 13:41:51 -0800

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
 #include "main.h"
-/**
- * print_alphabet_x10pprints the alphabet 10 times.
- *
- * return: always 0 (success)
- */
-void print_alphabet_x10(void)
-{
-	int i;
-	int j = 10 * i;
 
-	for (i = 97; i < 122; i++)
+/**
+ * main - entry function
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	for (int i = 'a'; i <= 'z'; i++)
 	{
+		int j = 10 * i;
+
 		_putchar(j);
 		_putchar('\n');
 	}
-
+	return (0);
 }
