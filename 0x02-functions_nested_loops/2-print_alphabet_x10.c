72b6215eaa6d88bf32169a56d83fc847741635ce
Cappu123 <cappyalemayehu12@gmail.com>
2023-11-23 14:17:25 -0800

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,12 +12,12 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (i = '1'; i <= '10'; i++)
 	{
-		for (j = 1; i < 11; j++)
+		for (j = 'a'; i < 'z'; j++)
 		{
-			_putchar(i);
+			_putchar(j);
 		}
 		_putchar('\n');
-	}	
+	}
 }
