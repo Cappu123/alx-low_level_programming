7bf441be23d4d6123c5a0d6e08c98a6d17efd47b
Cappu123 <cappyalemayehu12@gmail.com>
2023-11-25 02:29:31 -0800

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,6 @@
+#include "main.h"
+#include "_putchar.c"
+
 /**
  * jack_bauer - function that prints every minute of the day, 24 hour clock
  * h = hour, m = minutes
@@ -5,30 +8,23 @@
  * for loop breaks before passing 24:00
  * Return: 24 hour clock line by line
  */
-#include "main.h"
 void jack_bauer(void)
 {
-	int m, h = 0;
+	int h, m;
 
 	while (h < 24)
 	{
-		if (h < 10)
-		{
-			_putchar('0' + h + ':');
-		}
-		_putchar(h + ':');
-		h++;
-
-		m = 0;
-
 		while (m < 60)
 		{
-			if (m < 10)
-			{
-				_putchar('0' + m);
-			}
-			_putchar(m);
-			m++;
+			_putchar(h/10);
+			_putchar(h % 10);
+			_putchar(':');
+			_putchar(m / 10);
+			_putchar(m % 10);
+			_putchar ('\n');
 		}
+		m++;
 	}
+	h++;
 }
+
